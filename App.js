81cc7e62a2017bc3c6999c93ee0ca3dda5294cca
Curtis Hansen <curtishansen14@gmail.com>
2018-04-06 13:48:38 -0500

    Set up user login

--- App.js
+++ App.js
@@ -3,11 +3,21 @@ import { StyleSheet, Text, View } from 'react-native';
 import {Card} from 'react-native-elements';
 import * as firebase from 'firebase';
 import Login from "./src/login/Login"
+import { FIREBASEAPIKEY } from "./src/components/APIKeys";
 
 
 
 export default class App extends React.Component {
   render() {
+    var config = {
+      apiKey: FIREBASEAPIKEY,
+      authDomain: "dayminder-bc7fb.firebaseapp.com",
+      databaseURL: "https://dayminder-bc7fb.firebaseio.com",
+      projectId: "dayminder-bc7fb",
+      storageBucket: "dayminder-bc7fb.appspot.com",
+      messagingSenderId: "1015466394311"
+    };
+    firebase.initializeApp(config);
     return (
       <View style={styles.container}>
       <Login/>
--- src/login/Login.js
+++ src/login/Login.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import * as firebase from 'firebase'
-import {View, Button, Text, StyleSheet, TextInput} from 'react-native'
+import { View, Button, Text, StyleSheet, TextInput, Image } from 'react-native'
+import {TextField} from 'react-native-material-textfield'
 
-export default class login extends React.Component{
-    render(){
-        return(
+
+export default class login extends React.Component {
+    state={
+        email:'',
+        password:''
+    };
+
+    onLoginPress(){
+        const {email, password} = this.state;
+        console.log('Login press function start')
+        firebase.auth().signInWithEmailAndPassword(email ,password)
+        .then(() => {
+            console.log("Logged in");
+        });
+    }
+    render() {
+        let {email} = this.state;
+        let {password} = this.state;
+        return (
             <View>
-                <Text>
-                    Login page
-                </Text>
+                <View>
+                    <Image />
+                    <TextField label ='Email' 
+                    value={email} 
+                    onChangeText={ (email) => this.setState({email})} />
+                    <TextField label ='Password'
+                    value={password}
+                    onChangeText= { (password) => this.setState({password})} />
+                    <Button title="Login"  onPress ={this.onLoginPress.bind(this)} />
+                    <Button title="Sign Up" onPress ={() => console.log("Sign Up Pressed")}/>
+                </View>
             </View>
         );
     }
