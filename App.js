da1919f786a3ee95534f0cde514eff9c5beda681
Curtis Hansen <curtishansen14@gmail.com>
2018-04-11 14:46:18 -0500

    drafted nav tabs

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {Card} from 'react-native-elements';
-import * as firebase from 'firebase';
-import Login from "./src/login/Login"
-import Home from "./src/home/Home"
-import Prescription from "./src/prescription/Prescription"
-import Stats from "./src/stats/Stats"
-import account from "./src/account/Account"
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Card } from "react-native-elements";
+import * as firebase from "firebase";
+import Login from "./src/login/Login";
+import Home from "./src/home/Home";
+import Prescription from "./src/prescription/Prescription";
+import Stats from "./src/stats/Stats";
+import account from "./src/account/Account";
 import { FIREBASEAPIKEY } from "./src/components/APIKeys";
-import {StackNavigator,} from 'react-navigation'
-
-
+import { StackNavigator } from "react-navigation";
+import { NavigationBar } from "react-native-navbar";
+import { TabNavigator, TabBarBottom } from "react-navigation";
+import Ionicons from 'react'
 
 export default class App extends React.Component {
   render() {
@@ -23,27 +24,61 @@ export default class App extends React.Component {
       messagingSenderId: "1015466394311"
     };
     firebase.initializeApp(config);
-    return (
-
-      <Router/> 
-    );
+    return <Router />;
+    //return <Tabs/>
   }
 }
 
-const Router = StackNavigator({
-  Login: { screen: Login},
-  Home: {screen: Home},
-  Stats: {screen: Stats},
-  Prescription: {screen: Prescription},
-  Acct: {screen: account}
+//TODO: Get Navigation bar
 
-})
+// const Tabs = TabNavigator(
+//   {
+//     Login: { screen: Login },
+//     Home: { screen: Home },
+//     Stats: { screen: Stats },
+//     Prescription: { screen: Prescription },
+//     Acct: { screen: account }
+// },
+// {
+//   navigationOptions: ({navigation}) => ({
+//     tabBarIcon: ({focus, tintColor}) => {
+//       const {routeName} = navigation.state;
+//       let iconName;
+//       if (routeName === 'Home') {
+//         iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+//       } else if (routeName === 'Stats') {
+//         iconName = `ios-options${focused ? '' : '-outline'}`;
+//       }
+
+//       return <Ionicons name ={iconName} size ={25} color={tintColor}/>;
+
+//     },
+//   }),
+//   tabBarOptions: {
+//     activeTintColor: 'tomato',
+//     inactiveTintColor: 'gray'
+//   },
+//   tabBarComponent: TabBarBottom,
+//   tabBarPosition: 'bottom',
+//   animationEnabled: false,
+//   swipeEnabled: false,
+// }
+
+// );
+
+const Router = StackNavigator({
+  Login: { screen: Login },
+  Home: { screen: Home },
+  Stats: { screen: Stats },
+  Prescription: { screen: Prescription },
+  Acct: { screen: account }
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
