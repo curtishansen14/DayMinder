57b3a086e661893a180ea234f7198ee32e834cb5
Curtis Hansen <curtishansen14@gmail.com>
2018-04-16 16:29:10 -0500

    Added Nav tabs to bottom

--- App.js
+++ App.js
@@ -11,74 +11,68 @@ import { FIREBASEAPIKEY } from "./src/components/APIKeys";
 import { StackNavigator } from "react-navigation";
 import { NavigationBar } from "react-native-navbar";
 import { TabNavigator, TabBarBottom } from "react-navigation";
-import Ionicons from 'react'
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default class App extends React.Component {
-  render() {
-    var config = {
-      apiKey: FIREBASEAPIKEY,
-      authDomain: "dayminder-bc7fb.firebaseapp.com",
-      databaseURL: "https://dayminder-bc7fb.firebaseio.com",
-      projectId: "dayminder-bc7fb",
-      storageBucket: "dayminder-bc7fb.appspot.com",
-      messagingSenderId: "1015466394311"
-    };
-    firebase.initializeApp(config);
-    return <Router />;
-    //return <Tabs/>
-  }
+ render() {
+   var config = {
+     apiKey: FIREBASEAPIKEY,
+     authDomain: "dayminder-bc7fb.firebaseapp.com",
+     databaseURL: "https://dayminder-bc7fb.firebaseio.com",
+     projectId: "dayminder-bc7fb",
+     storageBucket: "dayminder-bc7fb.appspot.com",
+     messagingSenderId: "1015466394311"
+   };
+   firebase.initializeApp(config);
+   return <Router />;
+ }
 }
 
-//TODO: Get Navigation bar
-
-// const Tabs = TabNavigator(
-//   {
-//     Login: { screen: Login },
-//     Home: { screen: Home },
-//     Stats: { screen: Stats },
-//     Prescription: { screen: Prescription },
-//     Acct: { screen: account }
-// },
-// {
-//   navigationOptions: ({navigation}) => ({
-//     tabBarIcon: ({focus, tintColor}) => {
-//       const {routeName} = navigation.state;
-//       let iconName;
-//       if (routeName === 'Home') {
-//         iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-//       } else if (routeName === 'Stats') {
-//         iconName = `ios-options${focused ? '' : '-outline'}`;
-//       }
-
-//       return <Ionicons name ={iconName} size ={25} color={tintColor}/>;
-
-//     },
-//   }),
-//   tabBarOptions: {
-//     activeTintColor: 'tomato',
-//     inactiveTintColor: 'gray'
-//   },
-//   tabBarComponent: TabBarBottom,
-//   tabBarPosition: 'bottom',
-//   animationEnabled: false,
-//   swipeEnabled: false,
-// }
-
-// );
+const Tabs = TabNavigator(
+ {
+   Home: { screen: Home },
+   Stats: { screen: Stats },
+   Prescription: { screen: Prescription },
+   Acct: { screen: account }
+ },
+ {
+   navigationOptions: ({ navigation }) => ({
+     tabBarIcon: ({ focused, tintColor }) => {
+       const { routeName } = navigation.state;
+       let iconName;
+       if (routeName === "Home") {
+         iconName = `ios-pulse${focused ? "" : "-outline"}`;
+       } else if (routeName === "Stats") {
+         iconName = `ios-stats${focused ? "" : "-outline"}`;
+       } else if (routeName === "Prescription") {
+         iconName = `ios-planet${focused ? "" : "-outline"}`;
+       } else if (routeName === "Acct") {
+         iconName = `ios-person${focused ? "" : "-outline"}`;
+       }
+       return <Ionicons name={iconName} size={35} color={tintColor} />;
+     }
+   }),
+   tabBarOptions: {
+     activeTintColor: "tomato",
+     inactiveTintColor: "gray"
+   },
+   tabBarComponent: TabBarBottom,
+   tabBarPosition: "bottom",
+   animationEnabled: false,
+   swipeEnabled: false
+ }
+);
 
 const Router = StackNavigator({
-  Login: { screen: Login },
-  Home: { screen: Home },
-  Stats: { screen: Stats },
-  Prescription: { screen: Prescription },
-  Acct: { screen: account }
+ Login: { screen: Login },
+ Home: { screen: Tabs }
 });
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+ container: {
+   flex: 1,
+   backgroundColor: "#fff",
+   alignItems: "center",
+   justifyContent: "center"
+ }
 });
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import * as firebase from 'firebase'
-import { View, Button, Text, StyleSheet, TextInput, Image, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native'
-import {TextField} from 'react-native-material-textfield'
-import {StackNavigator,} from 'react-navigation'
+import React from "react";
+import * as firebase from "firebase";
+import {
+ View,
+ Button,
+ Text,
+ StyleSheet,
+ TextInput,
+ Image,
+ KeyboardAvoidingView,
+ TouchableWithoutFeedback,
+ Keyboard
+} from "react-native";
+import { TextField } from "react-native-material-textfield";
+import { StackNavigator } from "react-navigation";
 
-export default class Home extends React.Component{
-    render(){
-        console.log('Hello Dylan')
-        return(
-            <View>
-                <Text>This little piggy went home</Text>
-            </View>
-        );
-}
-
-
+export default class Home extends React.Component {
+ render() {
+   console.log("Hello Dylan");
+   return (
+     <View>
+       <Text>This little piggy went home</Text>
+     </View>
+   );
+ }
 }
