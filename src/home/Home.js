6aa9c8f415f5cf9176119846a14f3620cf028ce5
Curtis Hansen <curtishansen14@gmail.com>
2018-04-26 14:39:39 -0500

    add submission to firebase

--- src/home/Home.js
+++ src/home/Home.js
@@ -15,17 +15,40 @@ import {
 } from "react-native";
 import { TextField } from "react-native-material-textfield";
 import { StackNavigator } from "react-navigation";
+import { Rating, AirbnbRating } from "react-native-ratings";
+
+async function getUserName() {
+  const {currentUser} = await firebase.auth();
+  console.log(currentUser);
+  let result = currentUser.uid;
+  console.log(result);
+  return result;
+  
+}
+
 
 export default class Home extends React.Component {
   state = {
-    choiceOne: "JavaScript",
-    choiceTwo: "",
-    choiceThree: ""
+    choiceOne: "Wake",
+    choiceTwo: "Wake",
+    choiceThree: "Wake",
+    dayRating: 0
   };
 
-  onPress() {
-    const { choiceOne, choiceTwo, choiceThree } = this.state;
+  async onPress() {
+    const { choiceOne, choiceTwo, choiceThree, rating } = this.state;
     console.log("Submitted");
+    var db = firebase.database();
+    let today = Date.now();
+    let userID = await getUserName();
+    db.ref('users/' + userID + 'days/' + today).set({
+      day: this.state
+    });
+  }
+
+  ratingCompleted(rating){
+    this.setState({dayRating: rating})
+    console.log('state' + this.state.rating);
   }
 
   render() {
@@ -33,107 +56,122 @@ export default class Home extends React.Component {
 
     return (
       <View>
-        <ImageBackground 
-        style = {{width: "100%"}}
-        source = {require('../images/DayMinder_Sunset.png')}> 
-        <Text>What did you do today?</Text>
-        <View>
-       
-          <View style={styles.container}>
-            <Picker
-              selectedValue={this.state.choiceOne}
-              style={{ height: 50, width: 200 }}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({ choiceOne: itemValue })
-              }
-            >
-              <Picker.Item label="Up Early" value="Wake" />
-              <Picker.Item label="Bed on time" value="Sleep" />
-              <Picker.Item label="Socialized" value="Soc" />
-              <Picker.Item label="Cooked" value="Cook" />
-              <Picker.Item label="Worked >8hr" value="OverWork" />
-              <Picker.Item label="Exercised" value="Exercise" />
-              <Picker.Item label="Read" value="Read" />
-              <Picker.Item label="Played Game" value ="Game"/>
-              <Picker.Item label="Played Instrument" value ="Music"/>
-              <Picker.Item label='Wrote' value='Write'/> 
-              <Picker.Item label='Watched TV' value='TV'/>     
-            </Picker>
-            <Picker
-              selectedValue={this.state.choiceTwo}
-              style={{ height: 50, width: 200 }}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({ choiceTwo: itemValue })
-              }
-            >
-              <Picker.Item label="Up Early" value="Wake" />
-              <Picker.Item label="Bed on time" value="Sleep" />
-              <Picker.Item label="Socialized" value="Soc" />
-              <Picker.Item label="Cooked" value="Cook" />
-              <Picker.Item label="Worked >8hr" value="OverWork" />
-              <Picker.Item label="Exercised" value="Exercise" />
-              <Picker.Item label="Read" value="Read" />
-              <Picker.Item label="Played Game" value ="Game"/>
-              <Picker.Item label="Played Instrument" value ="Music"/>
-              <Picker.Item label='Wrote' value='Write'/> 
-              <Picker.Item label='Watched TV' value='TV'/>              
-            </Picker>
+        <ImageBackground
+          style={{ width: "100%" }}
+          source={require("../images/DayMinder_Sunset.png")}
+        >
+          <Text style={styles.titleText}>What did you do today?</Text>
+          <View>
+            <View style={styles.container}>
+              <Picker
+                selectedValue={this.state.choiceOne}
+                style={{ height: 50, width: 200 }}
+                onValueChange={(itemValue, itemIndex) =>
+                  this.setState({ choiceOne: itemValue })
+                }
+              >
+                <Picker.Item label="Up Early" value="Wake" />
+                <Picker.Item label="Bed on time" value="Sleep" />
+                <Picker.Item label="Socialized" value="Soc" />
+                <Picker.Item label="Cooked" value="Cook" />
+                <Picker.Item label="Worked >8hr" value="OverWork" />
+                <Picker.Item label="Exercised" value="Exercise" />
+                <Picker.Item label="Read" value="Read" />
+                <Picker.Item label="Played Game" value="Game" />
+                <Picker.Item label="Played Instrument" value="Music" />
+                <Picker.Item label="Wrote" value="Write" />
+                <Picker.Item label="Watched TV" value="TV" />
+              </Picker>
+              <Picker
+                selectedValue={this.state.choiceTwo}
+                style={{ height: 50, width: 200 }}
+                onValueChange={(itemValue, itemIndex) =>
+                  this.setState({ choiceTwo: itemValue })
+                }
+              >
+                <Picker.Item label="Up Early" value="Wake" />
+                <Picker.Item label="Bed on time" value="Sleep" />
+                <Picker.Item label="Socialized" value="Soc" />
+                <Picker.Item label="Cooked" value="Cook" />
+                <Picker.Item label="Worked >8hr" value="OverWork" />
+                <Picker.Item label="Exercised" value="Exercise" />
+                <Picker.Item label="Read" value="Read" />
+                <Picker.Item label="Played Game" value="Game" />
+                <Picker.Item label="Played Instrument" value="Music" />
+                <Picker.Item label="Wrote" value="Write" />
+                <Picker.Item label="Watched TV" value="TV" />
+              </Picker>
             </View>
 
-        <View style= {styles.containerCentered}>
-            <Picker
-              selectedValue={this.state.choiceThree}
-              style={{ height: 50, width: 200 }}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({ choiceThree: itemValue })
-              }
-            >
-              <Picker.Item label="Up Early" value="Wake" />
-              <Picker.Item label="Bed on time" value="Sleep" />
-              <Picker.Item label="Socialized" value="Soc" />
-              <Picker.Item label="Cooked" value="Cook" />
-              <Picker.Item label="Worked >8hr" value="OverWork" />
-              <Picker.Item label="Exercised" value="Exercise" />
-              <Picker.Item label="Read" value="Read" />
-              <Picker.Item label="Played Game" value ="Game"/>
-              <Picker.Item label="Played Instrument" value ="Music"/>
-              <Picker.Item label='Wrote' value='Write'/> 
-              <Picker.Item label='Watched TV' value='TV'/>     
-            </Picker>
+            <View style={styles.containerCentered}>
+              <Picker
+                selectedValue={this.state.choiceThree}
+                style={{ height: 50, width: 200 }}
+                onValueChange={(itemValue, itemIndex) =>
+                  this.setState({ choiceThree: itemValue })
+                }
+              >
+                <Picker.Item label="Up Early" value="Wake" />
+                <Picker.Item label="Bed on time" value="Sleep" />
+                <Picker.Item label="Socialized" value="Soc" />
+                <Picker.Item label="Cooked" value="Cook" />
+                <Picker.Item label="Worked >8hr" value="OverWork" />
+                <Picker.Item label="Exercised" value="Exercise" />
+                <Picker.Item label="Read" value="Read" />
+                <Picker.Item label="Played Game" value="Game" />
+                <Picker.Item label="Played Instrument" value="Music" />
+                <Picker.Item label="Wrote" value="Write" />
+                <Picker.Item label="Watched TV" value="TV" />
+              </Picker>
             </View>
-            
           </View>
-        
-    
-  
         </ImageBackground>
+
+        <View style={styles.rateStyle}>
+          <Rating
+            type="heart"
+            ratingCount={7}
+            imageSize={45}
+            onFinishRating={this.ratingCompleted.bind(this)}
+          />
+        </View>
+
         <View style={styles.buttonStyle}>
-        <Button
+          <Button
             title="Submit"
             color="#009a00"
             onPress={this.onPress.bind(this)}
           />
-        </View> 
+        </View>
       </View>
     );
   }
 }
 
-
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
+    flexDirection: "row",
     height: 200,
-    justifyContent: 'space-between'
+    justifyContent: "space-between"
   },
-  containerCentered : {
-    flexDirection: 'row',
+  containerCentered: {
+    flexDirection: "row",
     height: 200,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
 
-  buttonStyle:{
-    marginTop: 20
-  } 
-
-})
+  buttonStyle: {
+    marginTop: 10
+  },
+  rateStyle: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  titleText:{
+    fontFamily: 'Georgia',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    backgroundColor: '#009a00'}
+});
